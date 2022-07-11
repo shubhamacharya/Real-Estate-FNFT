daf101fd8b23a66103267debc7eb31a45b9c23ae
Shubham Acharya <shubhamacharya20@gmail.com>
2022-07-11 14:34:12 +0530

    feat : Added contracts and test cases.

--- blockchain/migrations/1_initial_migration.js
+++ blockchain/migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
-const Migrations = artifacts.require("Migrations");
+const FractionalNFT = artifacts.require("FractionalNFT");
+const FNFToken = artifacts.require("FNFToken");
+//const FractionalClaim = artifacts.require("FractionalClaim")
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(FractionalNFT);
+  deployer.deploy(FNFToken);
+  //deployer.deploy(FractionalClaim);
 };
