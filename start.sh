edfdbca11a39a0b96fe1b7ea9dd926bbd3491844
Shubham Acharya <shubhamacharya09@gmail.com>
2022-12-07 14:58:07 +0530

    feat: Shell script to start all project services.

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+    cd ./server
+    gnome-terminal --tab --title='Node Server' -- npm start
+    node = $$
+    #gnome-terminal --tab --title='Ganache' -- ganache -m "alert recall gain erupt clerk weekend stem direct chef grocery bone make" -l 3000000000
+    #ganache = $$
+    #cd ./public/images/NFT
+    #gnome-terminal --tab --title='Image Server' -- 'http-server'
+    #http = $$
+
+    echo $node
+    #echo $ganache
+    #echo $http
